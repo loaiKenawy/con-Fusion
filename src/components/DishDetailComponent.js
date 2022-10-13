601e3a1cdb6994d78924a8e12bd3dfda6c91d7d5
Loai Sayed Kenawy <77583369+loaiKenawy@users.noreply.github.com>
2022-10-13 22:52:28 +0200

    Thunk

--- src/components/DishDetailComponent.js
+++ src/components/DishDetailComponent.js
@@ -7,7 +7,7 @@ import {
 } from 'reactstrap';
 import { Control, LocalForm, Errors } from 'react-redux-form';
 import { Link } from 'react-router-dom';
-
+import { Loading } from './LoadingComponent';
 
 
 const required = (val) => val && val.length;
@@ -133,48 +133,61 @@ class DishDetailComponent extends Component {
     }
 
     render() {
-        var dish = this.props.dish
-        if (dish != null)
+        if (this.props.isLoading) {
             return (
                 <div className='container'>
-                    <div className="row">
-                        <Breadcrumb>
-                            <BreadcrumbItem><Link to="/home">Home</Link></BreadcrumbItem>
-                            <BreadcrumbItem><Link to="/menu">Menu</Link></BreadcrumbItem>
-                            <BreadcrumbItem active>{this.props.dish.name}</BreadcrumbItem>
-                        </Breadcrumb>
-                        <div className="col-12">
-                            <h3>{this.props.dish.name}</h3>
-                            <hr />
-                        </div>
-                    </div>
-                    <div className="row">
-                        <div className="col-12 col-md-5 m-1">
-                            <Card>
-                                <CardImg width="100%" src={dish.image} alt={dish.name} />
-                                <CardBody>
-                                    <CardTitle>{dish.name}</CardTitle>
-                                    <CardText>{dish.description}</CardText>
-                                </CardBody>
-                            </Card>
-                        </div>
-                        <div className="col-12 col-md-5 m-1" >
-                            <RenderComments comments={this.props.comments}
-                                addComment={this.props.addComment}
-                                dishId={this.props.dish.id}
-                            />
-                        </div>
+                    <div className='row'>
+                        <Loading />
                     </div>
                 </div>
             );
-        else {
-            console.log("DISH DET : ");
-            console.log(this.props.selectedDish);
         }
+        else if (this.props.errMess) {
+            return (
+                <div className='container'>
+                    <div className='row'>
+                        <h4>{this.props.errMess}</h4>
+                    </div>
+                </div>
+            );
+        }
+        else if (this.props.dish != null)
+        return (
+            <div className='container'>
+                <div className="row">
+                    <Breadcrumb>
+                        <BreadcrumbItem><Link to="/home">Home</Link></BreadcrumbItem>
+                        <BreadcrumbItem><Link to="/menu">Menu</Link></BreadcrumbItem>
+                        <BreadcrumbItem active>{this.props.dish.name}</BreadcrumbItem>
+                    </Breadcrumb>
+                    <div className="col-12">
+                        <h3>{this.props.dish.name}</h3>
+                        <hr />
+                    </div>
+                </div>
+                <div className="row">
+                    <div className="col-12 col-md-5 m-1">
+                        <Card>
+                            <CardImg width="100%" src={this.props.dish.image} alt={this.props.dish.name} />
+                            <CardBody>
+                                <CardTitle>{this.props.dish.name}</CardTitle>
+                                <CardText>{this.props.dish.description}</CardText>
+                            </CardBody>
+                        </Card>
+                    </div>
+                    <div className="col-12 col-md-5 m-1" >
+                        <RenderComments comments={this.props.comments}
+                            addComment={this.props.addComment}
+                            dishId={this.props.dish.id}
+                        />
+                    </div>
+                </div>
+            </div>
+        );
 
     }
 
-    // {this.renderComments(this.props.comments,this.props.addComment, this.props.dish.id)}
+    // {this.renderComments(this.props.comments, this.props.addComment, this.props.dish.id)}
     // {this.renderAddCommentForm()}
 }
 export default DishDetailComponent;
